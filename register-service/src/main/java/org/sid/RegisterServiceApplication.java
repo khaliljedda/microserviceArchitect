10fa29ec8f63721549f0f8dd5e02eb08edde2bb9
khaliljedda <khaliljedda4@gmail.com>
2020-03-28 23:30:06 +0100

    register service

---
+++ register-service/src/main/java/org/sid/RegisterServiceApplication.java
@@ -0,0 +1,14 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class RegisterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterServiceApplication.class, args);
+	}
+
+}
