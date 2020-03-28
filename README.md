72d8fc65f01e17ebb9862cda9acdfb60c88fea32
khaliljedda <khaliljedda4@gmail.com>
2020-03-29 00:43:14 +0100

    proxy service

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # microserviceArchitect
 microservice architect witdh springboot and spring cloud
+
+
+service company: 
+	dependecies: web,jpa,H2,RestRepository,lombok,config client,eureka client,actuator
+	port:8081
+
+config server:
+	dependecies:config server
+	port:8888
+
+registration server:
+	dependecies:Eureka server, clientConfig
+	port:8761
+
+proxy server:
+	dependecies:netflix zuul,  eurekaClient
+	port:8089
--- sevice-company/src/main/java/org/sid/SeviceCompanyApplication.java
+++ sevice-company/src/main/java/org/sid/SeviceCompanyApplication.java
@@ -7,8 +7,9 @@ import org.sid.entities.Company;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SeviceCompanyApplication {
 
